389c804f312ec3cf7618df636670eaef7735b66e
Yummy Yoshi <evergez25@gmail.com>
2023-04-22 20:35:49 -0400

    Added functional cancel button in course view and create enrollment view. Added ability to search, edit, and remove courses.

--- MAUI.LearningManagement/Views/PersonDetailView.xaml.cs
+++ MAUI.LearningManagement/Views/PersonDetailView.xaml.cs
@@ -21,6 +21,11 @@ public partial class PersonDetailView : ContentPage
     {
         (BindingContext as PersonDetailViewModel).AddPerson();
     }
+
+    private void CancelClick(object sender, EventArgs e)
+    {
+        Shell.Current.GoToAsync("//Instructor");
+    }
     private void OnLeaving(object sender, NavigatedFromEventArgs e)
     {
         BindingContext = null;
@@ -29,6 +34,5 @@ public partial class PersonDetailView : ContentPage
     private void OnArriving(object sender, NavigatedToEventArgs e)
     {
         BindingContext = new PersonDetailViewModel(PersonId);
-
     }
 }
